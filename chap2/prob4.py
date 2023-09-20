05477343b0c2e02abe1cdbd6da5dfdeb229f8551
cjw <wjddn0817@cu.ac.kr>
2023-09-20 02:59:05 +0000

    problem

---
+++ chap2/prob4.py
@@ -0,0 +1,6 @@
+name = input("Hi what's your name? ")
+age = int(input("And how old are you? "))
+weigh = int(input("Okay, last question. How many pounds do you weigh? ")
+print("\n")
+print("Did you know that you're just 4 in dog year?")
+print("But you're also over 883
