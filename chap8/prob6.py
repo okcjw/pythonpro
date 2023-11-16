63217e631bb4e58e1b02674bbdb9fd586267b34d
cjw <wjddn0817@cu.ac.kr>
2023-11-16 04:11:56 +0000

    chap8 python file added

--- chap8/prob6.py
+++ chap8/prob6.py
@@ -1,36 +1,83 @@
-# Trivia Challenge Game
 import random
-import pickle
 
-# Load the data file
-file = open("trivia_data.pkl", "rb")
-data = pickle.load(file)
-file.close()
+def load_data(file_path):
+    trivia_data = []
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
 
-# Get the number of questions
-num_questions = len(data)
+    i = 0
+    while i + 8 <= len(lines):
+        trivia_item = {}
+        title = lines[i].strip()
+        category = lines[i + 2].strip()
+        question = lines[i + 3].strip()
+        answers = [lines[i + j].strip() for j in range(4, 8)]
+        correct_answer = lines[i + 8].strip()
+        explanation = lines[i + 9].strip()
 
-# Choose a random question
-index = random.randint(0, num_questions - 1)
-question = data[index]
+        trivia_item['title'] = title
+        trivia_item['category'] = category
+        trivia_item['question'] = question
+        trivia_item['answers'] = answers
+        trivia_item['correct_answer'] = correct_answer
+        trivia_item['explanation'] = explanation
 
-# Display the question and the choices
-print("Welcome to Trivia Challenge!\n")
-print(question["title"])
-print(question["category"])
-print(question["question"])
-print()
-for i in range(1, 5):
-    print(f"{i} - {question['answer ' + str(i)]}")
-print()
+        trivia_data.append(trivia_item)
+        i += 11
 
-# Get the user's answer
-answer = int(input("What's your answer? "))
+    return trivia_data
 
-# Check the answer and display the result
-if answer == question["correct answer"]:
-    print("That's correct!")
-else:
-    print("Sorry, that's wrong.")
-print(question["explanation"])
+def display_question(trivia_item):
+    print('\tTitle: ' + trivia_item['title'])
+    print('---------------------------------------')
+    print('Category: ' + trivia_item['category'])
+    print('Question: ' + trivia_item['question'])
+    
+    for i, answer in enumerate(trivia_item['answers'], 1):
+        print(f"{i}. {answer}")
+
+    print()
+
+def get_user_answer():
+    while True:
+        try:
+            answer = input('Enter your answer (1-4) or type "exit" to end the game: ')
+            if answer.lower() == 'exit':
+                return 'exit'
+            answer = int(answer)
+            if 1 <= answer <= 4:
+                return answer
+            else:
+                print('Invalid input. Please enter a number between 1 and 4.')
+        except ValueError:
+            print('Invalid input. Please enter a number.')
+
+def check_answer(user_answer, correct_answer):
+    return str(user_answer) == correct_answer
+
+def main():
+    file_path = 'trivia_data.txt'
+    trivia_data = load_data(file_path)
+    if not trivia_data:
+        print("No trivia data found. Please check your file.")
+        return
+    
+    while True:
+        print()
+        random_question = random.choice(trivia_data)
+
+        display_question(random_question)
+        user_answer = get_user_answer()
+
+        if user_answer == 'exit':
+            print('Exiting the game. Goodbye!')
+            exit()
+    
+        if check_answer(user_answer, random_question['correct_answer']):
+            print('Correct! the answer number is ' + random_question['correct_answer'] + '\n' + random_question['explanation'])
+        else:
+            print('Incorrect! the answer number is ' + random_question['correct_answer'] + '\n' + random_question['explanation'])
+        
+if __name__ == "__main__":
+    main()
 
